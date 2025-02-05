7d31aa09f95399ab094163d996d4ed0cee5a4d75
Ana <anapedrabcn@gmail.com>
2025-02-05 14:36:30 +0100

    updated internal nav tabs, username logic

--- components/header.jsx
+++ components/header.jsx
@@ -4,8 +4,11 @@ import { Button } from "./ui/button";
 import { PenBox } from "lucide-react";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import UserMenu from ".//user-menu";
+import { checkUser } from "@/lib/checkUser";
 
-const Header = () => {
+async function Header() {
+  await checkUser();
+
   return (
     <nav className="mx-auto py-2 px-4 flex justify-between items-center shadow-md border-b-2">
       <Link href={"/"} className="flex items-center">
@@ -36,6 +39,6 @@ const Header = () => {
       </div>
     </nav>
   );
-};
+}
 
 export default Header;
