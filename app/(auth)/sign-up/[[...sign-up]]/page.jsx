2e9741e1a3f323a351c2690cdeaad7882370b44b
Ana <anapedrabcn@gmail.com>
2025-02-05 02:35:24 +0100

    added clerk

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { SignUp } from "@clerk/nextjs";
+
+const page = () => {
+  return <div>page</div>;
+};
+
+export default page;
---
+++ components/header.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { PenBox } from "lucide-react";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+const Header = () => {
+  return (
+    <nav className="mx-auto py-2 px-4 flex justify-between items-center shadow-md border-b-2">
+      <Link href={"/"} className="flex items-center">
+        <Image
+          src="/logo.png"
+          width="150"
+          height="60"
+          alt="Schedulrr Logo"
+          className="h-16 w-auto"
+        />
+      </Link>
+
+      <div className="flex items-center gap-4">
+        <Link href="/events?create=true">
+          <Button className="flex items-center gap-2">
+            <PenBox size={18} />
+            Create Event
+          </Button>
+        </Link>
+        <SignedOut>
+          <SignInButton forceRedirectUrl="/dashboard">
+            <Button variant="outline">Login</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
